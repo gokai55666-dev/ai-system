2173391dc117e027bef7840551c9e9f973f64cb8
gokai55666-dev <gokai55666@gmail.com>
2026-03-19 04:05:49 +1100

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,1 +1,10 @@
-#
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo "Updating system..."
+pkg update -y && pkg upgrade -y
+
+echo "Installing python deps..."
+pip install --upgrade pip
+pip install -r requirements.txt
+
+echo "Setup complete."
